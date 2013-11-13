8329e150c44d581b88352eb890ef077b8b204b66
JeffLego <jeff@legoservices.com>
2013-11-13 12:28:08 -0500

    changed home_path to root_path on static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,15 +6,15 @@ describe "Static pages" do
 	
   describe "Home page" do
     it "should have the content 'Sample App'" do
-    	visit home_path
+    	visit root_path
     	expect(page).to have_content('Sample App')
     end
     it "should have the title 'Home'" do
-    	visit home_path
+    	visit root_path
     	expect(page).to have_title("Ruby on Rails Tutorial Sample App")
    	end
     it "should not have a custom page title" do
-      visit home_path
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
