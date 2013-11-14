115ba797e559410192160cd17e18e8a12edc8519
JeffLego <jeff@legoservices.com>
2013-11-14 10:48:05 -0500

    added tests for the links on the layout by expecting the page to have specific titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,7 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let (:heading) { 'Sample app' }
+    let (:heading) { 'Sample App' }
     let (:page_title) { '' }
 
     it_should_behave_like "all static pages"
@@ -42,6 +42,19 @@ describe "Static pages" do
     let(:page_title) { 'Contact' }
 
     it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About us'))
+    click_link "Help"
+    expect(page).to have_title(full_title("Help"))
+    click_link "Contact"
+    expect(page).to have_title(full_title("Contact"))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title("Sign up"))
   end
 
 end
