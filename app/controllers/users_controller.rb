05a54207943ee4331170d9643553fabd351d4243
JeffLego <jeff@legoservices.com>
2013-11-20 10:37:11 -0500

    created an action that can handle signup failure, but not success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	def create
+		@user = User.new(params[:user]) #not the final implementation!
+		if @user.save
+			#handle a successful save.
+		else
+			render 'new'
+		end
+	end
 end
