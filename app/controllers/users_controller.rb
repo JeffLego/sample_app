9639319d1b6374170f52d8e5f505aed61c53518f
JeffLego <jeff@legoservices.com>
2013-11-20 11:29:27 -0500

    added error messages with css, added the redirect for a valid signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,18 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(params[:user]) #not the final implementation!
+		@user = User.new(user_params) #not the final implementation!
 		if @user.save
-			#handle a successful save.
+			redirect_to @user
 		else
 			render 'new'
 		end
 	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+		end
 end
